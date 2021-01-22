fb3eeae8255baa7f1b606ff121c2a2dc44bf4f4e
Jonas <jonas.verhenne@student.howest.be>
2021-01-22 12:03:44 +0100

    Updated direct method functions (working now)

---
+++ SendDirectMethod.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices;
+using System.Text;
+using Microsoft.Azure.Devices.Client;
+using System.Collections.Generic;
+
+namespace CloudFunctions
+{
+    public static class SendDirectMethod
+    {
+        [FunctionName("SendDirectMethod-UpdateStatus")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "status/{deviceId}")] HttpRequest req, string deviceId,
+            ILogger log)
+        {
+            // enable / disable security system
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+                //body van post lezen
+                string body = await new StreamReader(req.Body).ReadToEndAsync();
+                //var json = Newtonsoft.Json.JsonConvert.DeserializeObject(body);
+
+                //via ServiceClient communiceren met iot hub
+                ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(Environment.GetEnvironmentVariable("iotHubEdgeDeviceConnectionString"));
+
+                //maak CloudToDeviceMethod aan en payload invullen
+                CloudToDeviceMethod method = new CloudToDeviceMethod("updateStatus");
+                method.SetPayloadJson(body);
+
+                //naar device sturen
+                var response = await serviceClient.InvokeDeviceMethodAsync(deviceId, "EdgeModule", method);
+            }
+            catch (Exception ex)
+            {
+                log.LogError("error: " + ex);
+                throw;
+            }
+
+            return new OkObjectResult(200);
+        }
+
+        [FunctionName("SendDirectMethod-GetStatus")]
+        public static async Task<string> RunGet(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "status/{deviceId}")] HttpRequest req, string deviceId,
+            ILogger log)
+        {
+            // get status of security system
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            string status = "";
+            try
+            {
+                //via ServiceClient communiceren met iot hub
+                ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(Environment.GetEnvironmentVariable("iotHubEdgeDeviceConnectionString"));
+
+                //maak CloudToDeviceMethod aan en payload invullen
+                CloudToDeviceMethod method = new CloudToDeviceMethod("getStatus");
+
+                //naar device sturen
+                var response = await serviceClient.InvokeDeviceMethodAsync(deviceId, "EdgeModule", method);
+                var jsonResponse = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.GetPayloadAsJson());
+                log.LogInformation(jsonResponse["Status"]);
+                status = jsonResponse["Status"];
+            }
+            catch (Exception ex)
+            {
+                log.LogError("error: "+ex);
+                throw;
+            }
+            
+            return status;
+        }
+    }
+}
